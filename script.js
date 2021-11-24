9bdbe98c0ca84fedc4875d70a8aced7d6896373c
GXT <test@MacBook.local>
2021-11-24 23:31:03 +0900

    adding stric mode

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+let hasDriverLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 534;
