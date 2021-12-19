556d4e6e787f23d12489c86048a31c2fd78028f7
GXT <test@MacBook.local>
2021-12-19 16:12:13 +0900

    adding while Loop

--- script.js
+++ script.js
@@ -471,7 +471,7 @@ for (let i = 0; i < jonas.length; i++) {
 
   console.log(jonas[i], typeof jonas[i]);
 }*/
-
+/*
 const jonas = [
   "Jonas",
   "Schmedtmann",
@@ -493,4 +493,14 @@ for (let exercise = 1; exercise < 4; exercise++) {
   for (let rep = 1; rep < 6; rep++) {
     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
   }
+}*/
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+  console.log(`WHILE: Lifting weights repetition ${rep}`);
+  rep++;
 }
